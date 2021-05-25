92a0b05259e34b02390e71743bcba7d7a5d1fe51
Hardik Grover <hardikgrover@pop-os.localdomain>
2021-05-26 01:03:32 +0530

    footer finished

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,50 @@
 import React from "react";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
 import "./Footer.css";
+import { Grid, Slider } from "@material-ui/core";
 
 function Footer() {
   return (
     <div className="footer">
-      <h3> i am footer</h3>
+      <div className="footer_left">
+        <img
+          className="footer_albumLogo"
+          src="https://upload.wikimedia.org/wikipedia/en/2/2c/Levelssong.jpg"
+          alt=""
+        ></img>
+        <div className="footer_songInfo">
+          <h4>Avicci</h4>
+          <p>song info</p>
+          {/* <p>{item.artists.map((artist) => artist.name).join(", ")}</p> */}
+        </div>
+      </div>
+      <div className="footer_center">
+        <ShuffleIcon className="footer_green" />
+        <SkipPreviousIcon className="footer_icon" />
+        <PlayCircleOutlineIcon className="footer_icon" fontSize="large" />
+        <SkipNextIcon className="footer_green"></SkipNextIcon>
+        <RepeatIcon className="footer_green"></RepeatIcon>
+      </div>
+      <div className="footer_right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledby="continuous-slider" />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
